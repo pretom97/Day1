97a7ae2ec29915243b127ef415bd0af7a3a6de03
pretom97 <56485431+pretom97@users.noreply.github.com>
2019-10-15 17:02:00 +0600

    Add files via upload

---
+++ Day1/lesson1.py
@@ -0,0 +1,10 @@
+print('welcome to python class')
+print('ekn hatu pani')
+
+p = 'we love DIA'
+
+First_name= input('Enter your first name :')
+Last_name= input('Enter your Last name :')
+print('Hellow Mr.'+First_name.title()+ ' ' +Last_name.title())
+
+print(p.upper())
